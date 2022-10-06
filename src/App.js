54c0f74588a8679879db545a4797810d9f5ba620
LomejaneBabao <lomejanebabao37@gmail.com>
2022-10-07 07:41:08 +0800

    added files for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import OwnersAccount from './helpers/OwnersAccount';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +15,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />          
+          <Route path="/signup" element={<Signup />} />          
+          <Route path="/login" element={<Login />} />          
+          <Route path="/modal" element={<Modal />} /> 
+          <Route path="/OwnersAccount" element={<OwnersAccount />} />       
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/About.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import AboutImage from '../assets/torresur_hero2.jpg';
+import '../styles/About.css';
+
+function About() {
+  return (
+
+    <>
+
+      <div className="about">
+        <div className="aboutTop" style={{backgroundImage:`url(${AboutImage})`}}></div>
+        <div className="aboutBottom">
+          <h1>About Our Company</h1>
+          <p>
+            LEM Properties was founded in late 2022 by a triumvirate, consisting of Michael from Luzon, Enerio from Visayas and Lomejane from Mindanao, who have the passion in helping customers in the property sector regardless if they are a seller or a buyer. <br/> <br/>As a seller, you don't have to worry about your property not getting sold as our brokers are actively pursuing to find propective buyers. <br/> <br/>As a buyer, rest assured you don't get trapped into a bad deal as we provide legal services, if you need one, during the negotiation process until the deal is consummated.
+          </p>
+        </div>
+      </div>
+
+      <div className="OurVision">
+        <h1>Our Vision</h1>
+        <p>
+          To be one of the most favored property management company in the Philippine.
+        </p>
+      </div>
+
+      <div className="OurMission">
+        <h1>Our Mission</h1>
+        <p>
+          <ol>
+            <li>
+              Develop this website in consideration with customers from different demographics and regions in mind.
+            </li>
+            <li>
+              Increase the customer base thru digital marketing and thru mass-media ads.
+            </li>
+          </ol>
+        </p>
+      </div>
+
+    </>
+   
+  )
+}
+
+export default About
