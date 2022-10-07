c80a64deb76ec07c44a1e6a29a5e8d9c117becd1
LomejaneBabao <lomejanebabao37@gmail.com>
2022-10-07 08:53:26 +0800

    added some files from previous repository

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Modal from './components/Modal';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
